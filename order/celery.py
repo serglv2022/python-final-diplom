a1bcc59eccb8856562623ce9348eb887df6bdc93
serglv2022 <104458932+serglv2022@users.noreply.github.com>
2023-07-06 16:09:42 +0300

    Add files via upload

---
+++ order/celery.py
@@ -0,0 +1,10 @@
+import celery
+
+from order.settings import CELERY_BROKER_URL, CELERY_RESULT_BACKEND
+
+celery_app = celery.Celery(
+    broker=CELERY_BROKER_URL,
+    backend=CELERY_RESULT_BACKEND
+)
+
+celery_app.autodiscover_tasks()
